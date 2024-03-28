56c2a2a33295819fc241f1258152763ab0b78736
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-28 18:14:49 +0900

    Inital Commit

---
+++ board/src/board/Home.java
@@ -0,0 +1,39 @@
+package board;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Home {
+	
+	private Scanner sc;
+	private Random ran;
+	
+	public Home() {
+		sc = new Scanner(System.in);
+		ran = new Random();
+	}
+	
+	private String inputString(String message) {
+		System.out.print(message + " : ");
+		return sc.next();
+	}
+	
+	private int inputNumber(String message) {
+		int number = -1;
+		
+		System.out.print(message + " : ");
+		try {
+			String input = sc.next();
+			number = Integer.parseInt(input);
+		} catch (Exception e) {
+			System.err.println("숫자만 입력");
+		}
+		return number;	
+	}
+	
+	public void run() {
+		
+	}
+	
+	
+}
---
+++ board/src/board/User.java
@@ -0,0 +1,13 @@
+package board;
+
+public class User {
+	
+	private String name;
+	private int code;
+	
+	
+	public User(String name, int code) {
+		this.name = name;
+		this.code = code;
+	}
+}
