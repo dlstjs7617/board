69a37aeab3dc470d8c8ecaca479e6c4560d47903
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-29 12:46:09 +0900

    add createPost() 글작성 메소드 추가

--- board/src/board/PostManager.java
+++ board/src/board/PostManager.java
@@ -1,14 +1,25 @@
 package board;
 
+import java.util.ArrayList;
+
 public class PostManager {	
 	
+	ArrayList<Post> posts;
+	
 	private PostManager() {
-		
+		posts = new ArrayList<Post>();
 	}
 	
 	private static PostManager instance = new PostManager();
 	
 	public static PostManager getInstance() {
 		return instance;
+	}
+	
+	
+	public void createPost(User user, String title, String content) {
+		Post post = new Post(title, content, user.getName());
+		posts.add(post);
+		Board.map.put(user, posts);
 	}
 }
