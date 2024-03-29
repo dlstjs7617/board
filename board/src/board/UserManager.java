73d3ddac92c2f48094be7dec78386c3f0190c1bd
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-29 11:33:44 +0900

    add : userList 필드값 추가 findIdCheck : 아이디중복체크 메소드 추가

--- board/src/board/UserManager.java
+++ board/src/board/UserManager.java
@@ -1,10 +1,13 @@
 package board;
 
+import java.util.ArrayList;
+
 public class UserManager {
-	
+	ArrayList<User> userList;
 	
 	private UserManager() {
-		
+		userList = new ArrayList<>();
+		userList.add(Admin.getInstance());
 	}
 	
 	private static UserManager instance = new UserManager();
@@ -13,11 +16,23 @@ public class UserManager {
 		return instance;
 	}
 	
-	
-	
+	private boolean findIdCheck(String id) {
+		for(User user : userList) {
+			if(user.getId().equals(id))
+				return true;
+		}
+		return false;
+	}
 	
 	public User createUser(String name, String id, String password) {
+		if(findIdCheck(id)) {
+			System.err.println("이미 있는 아이디입니다.");
+			return null;
+		}
+		
 		User user = new User(name, id, password);
+		userList.add(user);
+		
 		return user;
 	}
 	
