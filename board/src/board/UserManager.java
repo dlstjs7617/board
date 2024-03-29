97365e2068c17236df50324beca24eca6b424cca
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-29 10:24:17 +0900

    add createUser() : 유저생성 메소드 추가

--- board/src/board/UserManager.java
+++ board/src/board/UserManager.java
@@ -1,5 +1,11 @@
 package board;
 
 public class UserManager {
-
+	
+	
+	public User createUser(String name, String id, String password) {
+		User user = new User(name, id, password);
+		return user;
+	}
+	
 }
