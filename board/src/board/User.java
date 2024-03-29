cee90f6b5ed2ca6924f8be3abc193a8090a4ad39
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-29 10:17:37 +0900

    add : Person클래스 상속

--- board/src/board/User.java
+++ board/src/board/User.java
@@ -1,13 +1,9 @@
 package board;
 
-public class User {
-	
-	private String name;
-	private int code;
-	
-	
-	public User(String name, int code) {
-		this.name = name;
-		this.code = code;
+public class User extends Person{
+
+	public User(String name, String id, String password) {
+		super(name, id, password);
 	}
+
 }
