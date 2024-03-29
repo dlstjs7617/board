77bbe188f56b836b5f35bd31d5bee3d0cf9c9766
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-29 17:02:57 +0900

    create :전체 게시글 조회를 위한 클래스 생성

---
+++ board/src/board/AllPost.java
@@ -0,0 +1,11 @@
+package board;
+
+import java.util.ArrayList;
+
+public class AllPost {
+	private static ArrayList<Post> post = new ArrayList<Post>();
+	
+	public static ArrayList<Post> getInstance(){
+		return post;
+	}
+}
