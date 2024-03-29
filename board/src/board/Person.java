188c2a379b87ac3f80f82949d2591a28f70bc457
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-03-29 10:15:05 +0900

    add : 필드값 과 생성자 추가

--- board/src/board/Person.java
+++ board/src/board/Person.java
@@ -2,4 +2,13 @@ package board;
 
 public abstract class Person {
 	
+	private String name;
+	private String id;
+	private String password;
+	
+	public Person(String name, String id, String password) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+	}
 }
